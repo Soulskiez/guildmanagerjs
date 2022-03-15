cae67d54d92da48214a84b9f5c5e2476eadc3312
Tyler Solnikowski <tylersolnikowski@pop-os.localdomain>
2022-03-14 21:04:10 -0400

    Added players to redis when game starts and sending list to UI

--- index.js
+++ index.js
@@ -11,14 +11,15 @@ const server = app.listen(port, console.log(`Server is running on port ${port}`)
 const io = new Server(server);
 
 io.on('connection', socket => {
-    connectToRedis();
-    socket.join('room 1');
-    Promise.all([fetchSockets()]).then((value) => {
+    const roomKey = 'room 1'; // Figure out Room key stuff later 
+    socket.join(roomKey);
+    Promise.all([fetchSockets(roomKey)]).then((value) => {
         // ATTENTION: This relies on one room existing.
         if(value[0].length >= 4) {
-            setTimeout(() => {
-                io.to('room 1').emit('game-start', {userIds: [], something: 1});
-            }, 3000);
+            beginGame(roomKey, value[0]);
+            // setTimeout(() => {
+            //     io.to('room 1').emit('game-start', {userIds: [], something: 1});
+            // }, 3000);
         }
     });
     
@@ -32,14 +33,51 @@ io.on('connection', socket => {
 
 });
 
-const fetchSockets = async () => {
-    const sockets = await io.in('room 1').fetchSockets();
+const fetchSockets = async roomKey => {
+    const sockets = await io.in(roomKey).fetchSockets();
     return sockets;
 }
 
 const connectToRedis = async () => {
     const client = createClient();
     client.on('error', (err) => console.log('Redis Client Error ', err));
-    await client.connect();
-    await client.mSet('keyTEst', 'testValue');
+    return client;
+    // await client.connect();
+    // await client.mSet('keyTEst', 'testValue');
 }
+
+const savePlayersByRoomId = async (roomKey, playersToSave) => {
+    const client = createClient();
+    await client.connect();
+    await client.set(roomKey, JSON.stringify(playersToSave));
+}
+
+const beginGame = (roomKey, connectedPlayers) => {
+    //io.to(roomKey).emit('game-start', {userIds: [], something: 1});
+    const playersToSave = connectedPlayers.map(player => (
+        {
+            id: player.id,
+            goldAmount: 5,
+            memberBank: [],
+            reputation: 0,
+        }
+    ));
+    savePlayersByRoomId(roomKey, playersToSave);
+    
+}
+
+
+// public string name; // Name of the Member
+// 	public float skill; // Skill level of the Member 1.0 - 20.0
+// 	public float alignment; // Alignment of good or evil -5.0 to 5.0  
+// 	public float experience; // Experience of raiding / quests 1.0 - 20.0// 	public float age; // Age of Member 16.0 - 45.0 Stats decline at 45.0 Member may choose to retire.
+// 	public float price;
+
+
+
+    // [] Create context for 4 players and their objects. (types for players)
+    // []When game starts add 5 coins to players banks, and send out update to UI.
+    // Then send them 5 player cards to decide if they want them.
+    // Await their responses 
+    //      It will come as user
+ 
